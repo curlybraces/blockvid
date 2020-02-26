8dfefa37df64c862cddc3cc3b44276291ff2eceb
Samuele Cavalleri <samuelecavalleri@gmail.com>
2020-02-26 21:27:11 +0100

    fix(axios): updated endpoint

--- src/core/axios.js
+++ src/core/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const endpoint = "http://localhost:8000/api/blockvid/";
+const endpoint = "http://localhost:8000/api/v1/blockvid/";
 
 const axiosClient = axios.create({
     baseURL: endpoint
