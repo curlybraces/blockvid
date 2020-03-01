6e8d9de6d41a0a0751e0dd0e50c159404da2a1f9
Samuele Cavalleri <samuelecavalleri@gmail.com>
2020-03-01 16:51:54 +0100

    perf(mobile): changes to run smoother on mobile devices

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -54,37 +54,6 @@
       </div>
       <div class="q-pt-md">
         <iframe
-          src="https://public.flourish.studio/visualisation/1462461/embed"
-          frameborder="0"
-          scrolling="no"
-          height="600"
-          marginheight="0"
-          class="full-width"
-          style="margin; 0"
-          title="Gedi Visual Map"
-        ></iframe>
-        <q-btn
-          type="a"
-          href="https://lab.gedidigital.it/gedi-visual/2020/coronavirus-i-contagi-in-italia/"
-          target="_blank"
-          label="Leggi altro"
-          flat
-          color="red-9"
-        />
-      </div>
-      <div class="q-pt-md">
-        <iframe
-          src="https://www.google.com/maps/d/embed?mid=17CmSRmkYLU2Zi0XQ0X1sO3ljqJTGEq0x&layer=tc&t=m"
-          height="600"
-          marginheight="0"
-          class="full-width"
-          style="margin; 0"
-          frameborder="0"
-          title="A map of countries with confirmed coronavirus cases, updated daily."
-        ></iframe>
-      </div>
-      <div class="q-pt-md">
-        <iframe
           src="https://dataviz.nbcnews.com/projects/20200122-coronavirus-world-count/index.html?initialWidth=760&amp;childId=embed-20200122-coronavirus-world-count&amp;parentTitle=Coronavirus%20map%3A%20The%20COVID-19%20virus%20is%20spreading%20across%20the%20world.%20Here%27s%20where%20cases%20have%20been%20confirmed.&amp;parentUrl=https%3A%2F%2Fwww.nbcnews.com%2Fhealth%2Fhealth-news%2Fcoronavirus-map-confirmed-cases-2020-n1120686"
           scrolling="no"
           marginheight="0"
@@ -203,30 +172,6 @@
           </div>
         </template>
       </q-table>
-    </div>
-
-    <div class="q-pt-lg full-width" style="max-width: 800px">
-      <div class="text-h6 text-red-9 no-padding">
-        <q-icon name="timeline" /> Grafici sul contagio
-      </div>
-      <iframe
-        src="https://dataviz.nbcnews.com/projects/20200122-coronavirus-history-count/index.html?initialWidth=760&amp;childId=embed-20200122-coronavirus-history-count&amp;parentTitle=Coronavirus%20map%3A%20The%20COVID-19%20virus%20is%20spreading%20across%20the%20world.%20Here%27s%20where%20cases%20have%20been%20confirmed.&amp;parentUrl=https%3A%2F%2Fwww.nbcnews.com%2Fhealth%2Fhealth-news%2Fcoronavirus-map-confirmed-cases-2020-n1120686"
-        scrolling="no"
-        marginheight="0"
-        frameborder="0"
-        height="3500px"
-        class="full-width auto-height"
-        title="Chart: Confirmed coronavirus cases, per country"
-        style="margin: 0;"
-      ></iframe>
-      <q-btn
-        type="a"
-        href="https://www.nbcnews.com/health/health-news/coronavirus-map-confirmed-cases-2020-n1120686"
-        target="_blank"
-        label="Leggi altro"
-        flat
-        color="red-9"
-      />
     </div>
 
     <div class="q-pt-lg full-width" style="max-width: 800px">
